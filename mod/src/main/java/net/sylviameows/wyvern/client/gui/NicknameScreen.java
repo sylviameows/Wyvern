977b4fc0de2bfa99a6d2976b0ab5efd6ef47c005
sylviameows <me@sylviameo.ws>
2025-12-30 21:55:08 -0600

    disabled nicknames button in matches

--- mod/src/main/java/net/sylviameows/wyvern/client/gui/NicknameScreen.java
+++ mod/src/main/java/net/sylviameows/wyvern/client/gui/NicknameScreen.java
@@ -1,6 +1,9 @@
 package net.sylviameows.wyvern.client.gui;
 
+import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.game.GameFunctions;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.sylviameows.wyvern.client.gui.widgets.WyvernButtonWidget;
@@ -12,6 +15,8 @@ public class NicknameScreen extends WyvernScreen {
 
     private final int GOLD_COLOR = 0xC09E42;
     private final PlayerEntity player;
+
+    private WyvernButtonWidget button;
 
     public NicknameScreen(PlayerEntity player) {
         super(Text.translatable("screen.wyvern.nickname"), 160, 100);
@@ -29,7 +34,13 @@ public class NicknameScreen extends WyvernScreen {
         NicknameComponent nickname = NicknameComponent.KEY.get(player);
         NicknameTextField field = new NicknameTextField(this.textRenderer, left() + 9, top() + this.textRenderer.fontHeight + 13, (unit * 2) - 2, (int) (this.textRenderer.fontHeight * 1.8), nickname);
         this.addDrawableChild(field);
-        this.addDrawableChild(new WyvernButtonWidget(left() + 9 + (unit * 2) + 2, top() + this.textRenderer.fontHeight + 13 - 1, unit - 2, (int) (this.textRenderer.fontHeight * 1.8) + 2, Text.translatable("wyvern.gui.set"), (a) -> ClientPlayNetworking.send(new NicknamePayload(field.getText()))));
+        this.button = new WyvernButtonWidget(left() + 9 + (unit * 2) + 2, top() + this.textRenderer.fontHeight + 13 - 1, unit - 2, (int) (this.textRenderer.fontHeight * 1.8) + 2, Text.translatable("wyvern.gui.set"), (a) -> ClientPlayNetworking.send(new NicknamePayload(field.getText())));
+        this.addDrawableChild(this.button);
     }
 
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        button.active = !GameWorldComponent.KEY.get(player.getWorld()).isRunning() || !GameFunctions.isPlayerAliveAndSurvival(player);
+        super.render(context, mouseX, mouseY, delta);
+    }
 }
